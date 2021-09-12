287cba71c20b2af76368b5bd4319e6546248642e
dgzornoza <dgzornoza@dgzornoza.com>
2021-09-12 20:33:06 +0200

    refactorizado proyecto y agregado proyecto .net core

---
+++ src/NetCore/Dynv6Updater/Program.cs
@@ -0,0 +1,32 @@
+﻿using Dynv6Updater.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace Dynv6Updater
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Execute();
+        }
+
+        private static async Task Execute()
+        {
+            try
+            {
+                Console.WriteLine("Actualizando IP ....");
+
+                IUpdateService updateService = new UpdaterService();
+                await _updaterService.UpdateIp();
+
+                Console.WriteLine("IP actualizada");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+    }
+}
